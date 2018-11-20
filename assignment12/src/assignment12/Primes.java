927c2b48d13984320cf879b29bc794d2816cc289
Nivedita Jakhar <Nivedita Jakhar@DESKTOP-G2H7B35>
2018-11-21 00:14:57 +0530

    assignment12_v1

---
+++ assignment12/src/assignment12/Primes.java
@@ -0,0 +1,104 @@
+package assignment12;
+
+
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Primes is a program that will compute prime numbers using the sieve of Eratosthenes.
+ * 
+ * @author Charles Hoot
+  * @version 4.0
+ */
+
+    
+public class Primes
+{	
+    public static void main(String args[])
+    {
+
+        int max;
+        
+        System.out.println("Please enter the maximum value to test for primality");
+        max = getInt("   It should be an integer value greater than or equal to 2.");
+        
+        /*creating three lists of candidates,composites and primes number*/
+        
+        ListInterface<Integer> candidates=new AList<Integer>();
+        ListInterface<Integer> composites=new AList<Integer>();
+        ListInterface<Integer> primes=new AList<Integer>();
+
+        
+        for(int i=2;i<=max;i++)
+        	candidates.add(i);
+        
+        System.out.println("candidates list : "+candidates);
+       
+        //checking each number 
+        
+        while(candidates.isEmpty()!=true) 
+        {
+        	int p=candidates.remove(1);
+        	primes.add(p);
+        	getComposites(candidates,composites,p);
+        }
+          
+        System.out.println("primes list : "+primes);
+        System.out.println("composites list : "+composites);
+    }
+    
+    
+    /**
+     * getComposites - Remove the composite values from possibles list and
+     * put them in the composites list.
+     *
+     * @param  candidates   A list of integers holding the possible values.
+     * @param  composites   A list of integers holding the composite values.
+     * @param  prime   An Integer that is prime.
+     */
+    public static void getComposites(ListInterface<Integer> candidates, ListInterface<Integer> composites, Integer prime)
+    {
+    	for(int i=1;i<=candidates.getLength();i++)
+        {
+    		if((candidates.getEntry(i))%prime==0)
+    			composites.add(candidates.remove(i)); //adding composite number to list by checking with prime no.
+        }
+    }
+    
+    
+    
+    
+    /**
+     * Get an integer value.
+     *
+     * @return     An integer. 
+     */
+    private static int getInt(String rangePrompt)
+    {
+        Scanner input;
+        int result = 10;        //Default value is 10
+        try
+        {
+            input = new Scanner(System.in);
+            System.out.println(rangePrompt);
+            result = input.nextInt();
+            
+        }
+        catch(NumberFormatException e)
+        {
+            System.out.println("Could not convert input to an integer");
+            System.out.println(e.getMessage());
+            System.out.println("Will use 10 as the default value");
+        }        
+        catch(Exception e)
+        {
+            System.out.println("There was an error with System.in");
+            System.out.println(e.getMessage());
+            System.out.println("Will use 10 as the default value");
+        }
+        return result;
+                                    
+    }    
+    
+}
